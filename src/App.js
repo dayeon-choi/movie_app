5c12ce884772c6b186d462d31ca87d7e61588d56
dayeon <dayeon2399@naver.com>
2021-03-11 21:53:29 +0900

    dayeon's commit - practice : function Food

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+function Food({fav}){
+  return <h1>I like {fav}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
-      Hello!
+      <h1>Hello!</h1>
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="samgiopsal"/>
+      <Food fav="shukumi"/>
     </div>
   );
 }
